df8b7f229f0fa0a447b8f3289ab0fb1d5edd1493
Charity <umercy001@gmail.com>
2022-09-13 13:46:03 +0100

    basic Calculator

---
+++ calculator.js
@@ -0,0 +1,10 @@
+calBody = document.getElementById("cal-body")
+
+function clearDisplay(){
+    document.getElementById("result").value = "";
+}
+
+function display(value){
+    document.getElementById("result").value += value;
+}
+
