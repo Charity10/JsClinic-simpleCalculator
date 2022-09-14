10bc927c3b12da1f64e35f4dc4228e5cdb3dc392
Charity <umercy001@gmail.com>
2022-09-14 20:30:05 +0100

    updated the calculator func

--- calculator.js
+++ calculator.js
@@ -1,10 +1,50 @@
-calBody = document.getElementById("cal-body")
+const display = document.getElementById( 'display-el')
+const controlButtons = document.getElementById( 'btn').children
 
-function clearDisplay(){
-    document.getElementById("result").value = "";
+
+const symbols = [ '+', '-', 'X', '+', '/', '%', 'C', '=']
+let firstValue = ""
+let secondValue =""
+let symbol = ""
+let result =''
+
+
+const calculate = () => {
+    firstValue = parseFloat(firstValue)
+    secondValue = parseFloat(secondValue)
+
+    if (symbol === '+') result = firstValue + secondValue
+    if (symbol === "-") result = firstValue - secondValue
+    if (symbol === "X") result = firstValue * secondValue
+    if (symbol === "/") result = firstValue / secondValue
+    if (symbol === "%") result = firstValue % secondValue
+
+
+    display.innerText = result
+    firstValue = result
+    secondValue = ''
 }
 
-function display(value){
-    document.getElementById("result").value += value;
+for(let button of controlButtons) {
+    button.addEventListener('click',  function() {
+        const { innerText: btnValue } = button
+        const btnValueIsSymbol = symbols.includes(btnValue)
+
+        if (!secondValue && btnValue === '=') return null
+       if (btnValue === 'C'){
+        firstValue= secondValue = symbol =''
+        return display.innerText =''
+       }
+        if (firstValue && btnValueIsSymbol) {
+            secondValue && calculate()
+            symbol = btnValue
+        } else if (!symbol) firstValue += btnValue
+
+        else if (symbol) secondValue += btnValue
+
+         if (btnValue !== "=") display.innerText += btnValue
+
+       
+    })
 }
 
